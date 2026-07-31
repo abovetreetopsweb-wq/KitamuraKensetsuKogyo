98e5fe2ac9bece6fc6ba9c915ea9578d9c0e03e6
abovetreetopsweb-wq <abovetreetops.web@gmail.com>
2026-07-31 11:21:21 +0900

    アニメーション他

--- js/main.js
+++ js/main.js
@@ -13,3 +13,78 @@ const swiper = new Swiper('.swiper',{
   delay: 3000,
 },
 });
+
+
+
+//モーダル
+$(".modal-open").click(
+    function(){
+        $(this).find(".modal-area").show()
+        $("body").addClass("is-modal-open");
+        goTop.hide();
+        header.hide();
+    }
+);
+
+$(".modal-close").click(
+    function(e){e.stopPropagation();
+        $(this).closest(".modal-area").hide()
+        $("body").removeClass("is-modal-open");
+        if ($(window).scrollTop() > 100) {
+    goTop.show();
+      header.show();
+}
+    }
+);
+
+
+
+
+//Topへ戻る
+//変数宣言と代入
+var goTop = $(".fas");
+
+//サイト上部ではボタンを非表示
+goTop.hide();
+
+//100pxスクロールしたらボタン表示・100px以下ならボタン非表示
+$(window).scroll(function(){
+    if ($(this).scrollTop()>100){
+        // 0.3でフェードイン
+    goTop.fadeIn(300);
+    }else{
+        //0.3秒でフェードアウト
+        goTop.fadeOut(300)
+    }
+});
+
+//ボタンがクリックされたら1秒でページトップへ戻る
+goTop.click(function(){
+    $("body,html").animate({scrollTop:0},1000);
+    return false;
+})
+
+
+// ヘッダーとABOUTを取得
+var header = $(".header");
+var about = $("#about");
+
+// スクロールしたとき
+$(window).scroll(function () {
+  // ABOUTの上端位置
+  var aboutTop = about.offset().top;
+
+  // 現在のスクロール位置
+  var scrollTop = $(this).scrollTop();
+
+  // ヘッダーの高さ
+  var headerHeight = header.outerHeight();
+
+  // ABOUTがヘッダーの下まで来たら背景色を付ける
+  //一時的にこうなる→<header class="header is-bg">
+  if (scrollTop >= aboutTop - headerHeight) {
+    header.addClass("is-bg");
+  } else {
+    header.removeClass("is-bg");
+  }
+});
